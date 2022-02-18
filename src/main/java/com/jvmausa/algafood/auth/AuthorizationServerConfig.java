76f29dd003b72094ecde0571e5478c0935bffa06
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2022-02-18 13:43:15 -0300

    configurações iniciais

---
+++ src/main/java/com/jvmausa/algafood/auth/AuthorizationServerConfig.java
@@ -0,0 +1,15 @@
+package com.jvmausa.algafood.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+
+	
+	
+	
+	
+}
