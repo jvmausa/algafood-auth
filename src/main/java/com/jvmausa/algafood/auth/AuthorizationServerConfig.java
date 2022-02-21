7f2c05d8616887cf0bba1f79b37165edfc8c8ffa
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2022-02-21 11:35:39 -0300

    configurando envio de access token

--- src/main/java/com/jvmausa/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/jvmausa/algafood/auth/AuthorizationServerConfig.java
@@ -1,15 +1,49 @@
 package com.jvmausa.algafood.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
 	
 	
+	/*COnfigurar os clients que podem acessar esse authorizationServer e depois vão acessar os recursos protegidos
+		no resource server(aplicação)*/
 	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+		.inMemory()
+			.withClient("algafood-web")
+			.secret(passwordEncoder.encode("web123"))
+			.authorizedGrantTypes("password")
+			.scopes("write", "read")
+			.accessTokenValiditySeconds(100)
+		.and()
+			.inMemory()
+			.withClient("app-mobile")
+			.secret(passwordEncoder.encode("web123"))
+			.authorizedGrantTypes("password")
+			.scopes("write", "read");
+	}
+	
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager);
+	}
 	
 }
--- src/main/java/com/jvmausa/algafood/auth/WebSecurityConfig.java
+++ src/main/java/com/jvmausa/algafood/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.jvmausa.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,4 +37,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
 }
